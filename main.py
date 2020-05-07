0ee21c1a6e63ffe388960dd1380b6fa19f1eaa36
A1anHou <1174814581@qq.com>
2020-05-07 10:17:25 +0800

    修改

--- main.py
+++ main.py
@@ -1,110 +1,105 @@
+import pandas as pd
+from transformers import BertTokenizer, BertForSequenceClassification, AdamW
+from sklearn.model_selection import train_test_split#将数据分为测试集和训练集
 import torch
-import torch.nn as nn
-
-import bertfcn
-import loaddata
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
+import numpy as np
+from tqdm import trange
+from torch import optim
 
 # 数据文件路径
 FILE_PATH = 'data/spam.csv'
-# 模型参数
-WEIGHT_PATH = 'bert-base-uncased'
-VOC_PATH = 'bert-base-uncased'
-
-# Dropout率
-DROP_OUT = 0.1
-# 学习率
-LEARNING_RATE = 1e-3
-
-# 训练批次大小
-BATCH_SIZE = 16
-# 训练轮数
-EPOCHS = 10
-# 训练设备
+# 模型和词表路径
+WEIGHT_PATH = 'data/bert_base_uncased'
+VOC_PATH = 'data/bert_base_uncased/bert-base-uncased-vocab.txt'
 DEVICE = torch.device('cuda:0')
 
+# 读取训练数据 os.path.join(data_dir, "train.txt")
+df = pd.read_csv(FILE_PATH)
+# 提取语句并处理
+labels = [0 if label == 'ham' else 1 for label in df.v1.values]
+print(df.v2.values[0])
+tokenizer = BertTokenizer.from_pretrained(VOC_PATH, do_lower_case=True)
+tokenized_sents = [tokenizer.encode(sent, max_length=128, pad_to_max_length=True) for sent in df.v2.values]
 
-# 训练函数
-def train(net, loader, criterion, optimizer):
-    # 遍历每个batch
-    for batch_idx, (data, target) in enumerate(loader):
-        # 如果计算机有NVIDIA GPU，则将数据放在GPU上训练
-        if torch.cuda.is_available():
-            data, target = data.to(DEVICE), target.to(DEVICE)
-        # 得到模型输入序列，大小为[batch_size, sequence_length]
-        input_seq = data.squeeze(dim=1)
-        # 得到标签序列，大小为[batch_size]
-        target = target.squeeze()
-        print(batch_idx)
-        # 正向传播得到结果
-        out = net(input_seq)
-        # 清零梯度
-        optimizer.zero_grad()
-        # 计算loss
-        loss = criterion(out, target)
-        # 反向传播
-        loss.backward()
-        # 优化参数
-        optimizer.step()
-        # 每5个batch输出一次训练情况
-        if batch_idx % 5 == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(epoch, batch_idx * len(data),
-                                                                           len(train_dataset),
-                                                                           100. * batch_idx / len(train_loader),
-                                                                           loss.item()))
+#划分训练集、验证集
+train_inputs, validation_inputs, train_labels, validation_labels = train_test_split(tokenized_sents, labels,
+                                                            random_state=2018, test_size=0.1)
 
+print("训练集的一个inputs",tokenizer.decode(train_inputs[0]))
 
-# 验证函数，注意验证过程不需要反向传播
-def eval(net, loader, criterion):
-    val_loss = 0
-    correct = 0
-    for batch_idx, (data, target) in enumerate(loader):
-        # 如果计算机有NVIDIA GPU，则将数据放在GPU上验证
-        if torch.cuda.is_available():
-            data, target = data.to(DEVICE), target.to(DEVICE)
-        input_seq = data.squeeze(dim=1)
-        target = target.squeeze()
-        # 正向传播得到结果
-        out = net(input_seq)
-        # 累加loss
-        val_loss += criterion(out, target).item()
-        # 将输出值最大的一项作为预测结果
-        pred = out.argmax()
-        # 累加预测正确的数目
-        correct += pred.eq(target.data).float().sum().item()
+#将训练集、验证集转化成tensor
+train_inputs = torch.tensor(train_inputs)
+validation_inputs = torch.tensor(validation_inputs)
+train_labels = torch.tensor(train_labels)
+validation_labels = torch.tensor(validation_labels)
 
-    # 计算平均loss
-    val_loss /= len(val_dataset)
-    print('\nVal set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(val_loss, correct, len(val_dataset),
-                                                                                100. * correct / len(val_dataset)))
+#生成dataloader
+batch_size = 32
+train_data = TensorDataset(train_inputs, train_labels)
+train_sampler = RandomSampler(train_data)
+train_dataloader = DataLoader(train_data, sampler=train_sampler, batch_size=batch_size)
+validation_data = TensorDataset(validation_inputs,  validation_labels)
+validation_sampler = SequentialSampler(validation_data)
+validation_dataloader = DataLoader(validation_data, sampler=validation_sampler, batch_size=batch_size)
 
 
-# 加载数据
-content_list, label_list = loaddata.read_data(FILE_PATH, VOC_PATH)
-# 建立数据集
-all_dataset = loaddata.SpamLabelDataset(content_list, label_list)
-# 将数据集划分为训练集、验证集和测试集
-train_dataset, val_dataset, test_dataset = torch.utils.data.random_split(all_dataset, [4000, 1000, 572])
+model = BertForSequenceClassification.from_pretrained(WEIGHT_PATH)
+print(model.cuda())
 
-# 建立批处理抽取器
-train_loader = loaddata.set_dataloader(train_dataset, BATCH_SIZE)
-val_loader = loaddata.set_dataloader(val_dataset, BATCH_SIZE)
-test_loader = loaddata.set_dataloader(test_dataset, BATCH_SIZE)
 
-# 实例化模型
-model = bertfcn.BertFcModel(WEIGHT_PATH, DROP_OUT)
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
+# param_optimizer = list(model.named_parameters())
+# no_decay = ['bias', 'gamma', 'beta']
+# optimizer_grouped_parameters = [
+#     {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)],
+#      'weight_decay_rate': 0.01},
+#     {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)],
+#      'weight_decay_rate': 0.0}
+# ]
 
-# 如果计算机有NVIDIA GPU，则将模型放在GPU上训练
-if torch.cuda.is_available():
-    model.to(DEVICE)
+# optimizer = AdamW(optimizer_grouped_parameters,
+#                      lr=2e-5)
+optimizer = optim.Adam(model.parameters(), lr=1e-2)
 
-# 开始训练
-for epoch in range(EPOCHS):
-    # 将模型切换为训练模式
+#定义一个计算准确率的函数
+def flat_accuracy(preds, labels):
+    pred_flat = np.argmax(preds, axis=1).flatten()
+    labels_flat = labels.flatten()
+    return np.sum(pred_flat == labels_flat) / len(labels_flat)
+
+#训练开始
+epochs = 10
+for _ in trange(epochs, desc="Epoch"):
+    #训练开始
     model.train()
-    train(model, train_loader, criterion, optimizer)
-    # 将模型切换为验证模式（在验证模式中模型不会进行Dropout操作）
+    tr_loss = 0
+    nb_tr_examples, nb_tr_steps = 0, 0
+    for step, batch in enumerate(train_dataloader):
+        batch = tuple(t.to(DEVICE) for t in batch)
+        b_input_ids, b_labels = batch
+        optimizer.zero_grad()
+        #取第一个位置，BertForSequenceClassification第一个位置是Loss，第二个位置是[CLS]的logits
+        loss = model(b_input_ids, token_type_ids=None,  labels=b_labels)[0]
+        loss.backward()
+        optimizer.step()
+
+        tr_loss += loss.item()
+        nb_tr_examples += b_input_ids.size(0)
+        nb_tr_steps += 1
+    print("Train loss: {}".format(tr_loss / nb_tr_steps))
+    #模型评估
     model.eval()
-    # 每训练一个epoch对模型性能进行一次验证
-    eval(model, val_loader, criterion)
+    eval_loss, eval_accuracy = 0, 0
+    nb_eval_steps, nb_eval_examples = 0, 0
+    for batch in validation_dataloader:
+        batch = tuple(t.to(DEVICE) for t in batch)
+        b_input_ids, b_labels = batch
+        with torch.no_grad():
+            logits = model(b_input_ids, token_type_ids=None)[0]
+        logits = logits.detach().cpu().numpy()
+        label_ids = b_labels.to('cpu').numpy()
+        tmp_eval_accuracy = flat_accuracy(logits, label_ids)
+        eval_accuracy += tmp_eval_accuracy
+        nb_eval_steps += 1
+    print("Validation Accuracy: {}".format(eval_accuracy / nb_eval_steps))
+
